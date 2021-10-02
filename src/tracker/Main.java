8f2174878982e42326f493802f978d2272417263
Ivan Khizhkin <ivan.khizhkin@gmail.com>
2021-10-02 19:48:57 +0300

    ДЗ2

---
+++ src/tracker/Main.java
@@ -0,0 +1,26 @@
+package tracker;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите название дефекта:");
+        String resume = scanner.nextLine();
+
+        System.out.println("Введите критичность дефекта - низкий, средний, высокий, блокер");
+        String critical = scanner.nextLine();
+
+        System.out.println("Введите ожидаемо количество дней на исправление:");
+        int countDays = scanner.nextInt();
+        String s = scanner.nextLine();
+
+        boolean howMuchTime = countDays > 7;
+
+        System.out.format("%32s%13s%32s%31s", "Резюме|", "Серьёзность|",
+                "Количество дней на исправление|", "Займет больше рабочей недели?" + "\n");
+        System.out.format("%32s%13s%32s%31s", resume + "|", critical + "|", countDays + "|", howMuchTime);
+
+    }
+}
