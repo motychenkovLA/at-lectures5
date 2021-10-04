aa3abad3f069621fc226bcaeb7d2c133a005bd32
spilipenko <sergey.pilipenko94@yandex.ru>
2021-10-05 00:19:02 +0300

    ДЗ 2

--- src/lecture_2/Main.java
+++ src/lecture_2/Main.java
@@ -1,7 +1,33 @@
 package lecture_2;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("tetst");
+
+        String summary;
+        String priority;
+        int numOfDays;
+        boolean duringWeek;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите резюме дефекта:");
+        summary = scanner.nextLine();
+
+        System.out.println("Введите критичность дефекта: блокирующий, высокий, средний, низкий:");
+        priority = scanner.nextLine();
+
+        System.out.println("Введите количество дней на исправление дефекта:");
+        numOfDays = scanner.nextInt();
+        scanner.nextLine();
+
+        duringWeek = 5 < numOfDays;
+
+        System.out.println("\nИнформация о дефекте");
+        System.out.println("Резюме: " + summary);
+        System.out.println("Критичность: " + priority);
+        System.out.println("Количество дней на исправление: " + numOfDays);
+        System.out.println("Займет больше рабочей недели: " + duringWeek);
     }
 }
