b9d25c5ecc4bb7749f22c079b3501970b26815fe
lmotychenkov <motychenkovLA@gmail.com>
2021-10-05 11:14:15 +0300

    дз 2 замечания

--- src/tracker/Main.java
+++ src/tracker/Main.java
@@ -13,14 +13,17 @@ public class Main {
         String priority = scanner.nextLine();
 
         System.out.println("Введите ожидаемое количество дней на исправление дефекта:");
+        // todo 0 лучше daysToFix
         int daysForFixed = scanner.nextInt();
         scanner.nextLine();
 
+        // todo 3 рабочая неделя 5 дней
+        // todo 3 лучше вынести число в константу в самое начало метода
         boolean moreThanWeek = 7 < daysForFixed;
 
         System.out.println("Информация о дефекте:");
+        // todo 1 строка вылезает за экран, можно перенести, разделив по плюсам
+        // todo 1 складываются литералы (вроде "a" + "b"), лучше сразу написать результат ("ab")
         System.out.println( "Резюме:" +  resume + "|"+ "Серьезность:" + priority + "|" + "Количество дней на исправление:" + daysForFixed + "|" + "Займет больше рабочей недели? " + moreThanWeek + "|");
-
-
     }
 }
