82b37b8af59fb85f567523546481c303132c2160
ilyaosin <osin.i.g@outlook.com>
2021-10-07 01:13:52 +0300

    ДЗ_3 Меню

--- src/Homework_3/Main.java
+++ src/Homework_3/Main.java
@@ -17,43 +17,54 @@ public class Main {
 
         while (!"quit".equals(menu)) {
             // Ввод команды
-            System.out.println("_____________________________________________");
+            System.out.println("*******************************************");
+            System.out.println();
             System.out.println("Введите команду, чтобы:" + '\n' + '\t' + "Добавить новый дефект: add" + '\n' + '\t' + "Вывести список дефектов: list" + '\n' + '\t' + "Выйти из программы: quit");
             System.out.print('\n' + "Введите команду: ");
             menu = scan.nextLine();
+            System.out.println();
 
-            for (int i = 0; i < 50; ++i) System.out.println(); // Костыльная очистка консоли через сдвиг за счет переносов строк
+            //for (int i = 0; i < 50; ++i) System.out.println(); // Костыльная очистка консоли через сдвиг за счет переносов строк
 
             switch (menu) {
                 case "add": // Добавление дефекта
                     if (defects < MAX_DEFECTS) {
 
                         // Ввод резюме
+                        System.out.println("*******************************************");
+                        System.out.println();
                         System.out.print("Резюме дефекта: ");
                         String resume = scan.nextLine();
                         listOfBugs[row][0] = resume;
+                        System.out.println();
 
                         // Ввод критичности
                         System.out.println("Введите кричисность дефекта:" + '\n' + '\t' + "- Blocker" + '\n' + '\t' + "- Critical" + '\n' + '\t' + "- Major" + '\n' + '\t' + "- Minor" + '\n' + '\t' + "- Trivial" + '\n');
                         System.out.print("Критичность: ");
                         String severity = scan.nextLine();
                         listOfBugs[row][1] = severity;
+                        System.out.println();
 
                         // Ввод количества дней на исправление
                         System.out.print("Ожидаемое кол-во дней на исправление: ");
                         int daysToFix = scan.nextInt();
                         String str = Integer.toString(daysToFix);
                         listOfBugs[row][2] = str;
+                        System.out.println();
 
                         row++;
                         defects++;
                         menu = null;
                     } else {
-                        System.out.println("Достигнуто максимальное количество дефектов. Возврат в главное меню...");
+                        System.out.println("*******************************************");
+                        System.out.println();
+                        System.out.println("ВНИМАНИЕ!!! Достигнуто максимальное количество дефектов. Возврат в главное меню...");
+                        System.out.println();
                     }
                     break;
                 case "list": // Вывод на экран списка созданных дефектов.
-                    System.out.println("_____________________________________________");
+                    System.out.println("*******************************************");
+                    System.out.println();
                     System.out.println("Список дефектов:");
                     for (row = 0; row < defects; row++) {
                         System.out.println(row + " | " + listOfBugs[row][0] + " | " + listOfBugs[row][1] + " | " + listOfBugs[row][2]);
