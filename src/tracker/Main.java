e137648b36f528ebc56b78c94694f2100eb45a1a
smakarova <svetlana.makarova.1992@list.ru>
2021-10-08 18:43:24 +0300

    ДЗ 2 (Исправления)

--- src/tracker/Main.java
+++ src/tracker/Main.java
@@ -1,29 +1,79 @@
 package tracker;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        String[] listBug = new String[10];
+        int countReq =  0; // переменная для количества дефектов
+        for (int i = 0; i < 12; i++) {
+
+            Scanner scanner = new Scanner(System.in);
 
 
-        final int WORK_DAYS = 5;
+            System.out.println(" Выберите действие Add (Новый дефект)" +
+                    " List (Вывести список дефектов)" +
 
-        Scanner scanner = new Scanner(System.in);
+                    " Quit (Вернуться в главное меню)");
+            String action = scanner.nextLine();
 
-        System.out.println("Введите резюме дефекта:");
-        String resume = scanner.nextLine();
+            switch (action) {
+                case "Add":
 
-        System.out.println("Введите критичность дефекта (Высокий, Средний, Низкий ):");
-        String priority = scanner.nextLine();
+                    if (countReq > 9) {
+                        System.out.println("Невозможно добавить больше 10 дефектов");
 
-        System.out.println("Введите ожидаемое количество дней на исправление дефекта:");
-        int daysToFixed = scanner.nextInt();
-        scanner.nextLine();
+                        break;
+                    }
+                    System.out.println("Введите резюме дефекта:");
+                    String resume = scanner.nextLine();
 
-        boolean moreThanWeek = WORK_DAYS < daysToFixed;
+                    System.out.println("Введите критичность дефекта (Высокий, Средний, Низкий ):");
+                    String priority = scanner.nextLine();
 
-        System.out.println("Информация о дефекте:");
-        System.out.println( "Резюме:" +  resume + "|Серьезность:" + priority + "|Количество дней на исправление:"
-                + daysToFixed + "|Займет больше рабочей недели? " + moreThanWeek + "|" );
+                    System.out.println("Введите ожидаемое количество дней на исправление дефекта:");
+                    int daysToFixed = scanner.nextInt();
+                    scanner.nextLine();
+                    String infoBug = "|"+ countReq + "|"+ resume + "|"+ priority+ "|" + daysToFixed+ "|" ;
+                    listBug[countReq]=infoBug;
+                    countReq = countReq + 1;
+                    break;
+                case "List":
+                  // System.out.println(Arrays.toString(listbug));
+
+                    for (String a: listBug) {
+                        System.out.println(a);
+                    }
+                    break;
+
+                case "Quit":
+                    break;
+                default:
+                    System.out.println("Операция не существует");
+                    break;
+            }
+
+
+            //final int WORK_DAYS = 5;
+
+            //Scanner scanner = new Scanner(System.in);
+
+            //System.out.println("Введите резюме дефекта:");
+            //String resume = scanner.nextLine();
+
+            //System.out.println("Введите критичность дефекта (Высокий, Средний, Низкий ):");
+            //String priority = scanner.nextLine();
+
+            //System.out.println("Введите ожидаемое количество дней на исправление дефекта:");
+            //int daysToFixed = scanner.nextInt();
+            //scanner.nextLine();
+
+            //boolean moreThanWeek = WORK_DAYS < daysToFixed;
+
+            // System.out.println("Информация о дефекте:");
+            //System.out.println( "Резюме:" +  resume + "|Серьезность:" + priority + "|Количество дней на исправление:"
+            //+ daysToFixed + "|Займет больше рабочей недели? " + moreThanWeek + "|" );
+        }
     }
 }
