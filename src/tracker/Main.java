57d3a742c495ab3588b5884edb81bc961c75888b
schupikova <victorovna.sveta@gmail.com>
2021-10-11 01:23:24 +0300

    ДЗ_3. Применен GoogleCodeStyle

--- src/tracker/Main.java
+++ src/tracker/Main.java
@@ -1,23 +1,48 @@
 package tracker;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
   public static void main(String[] args) {
-    final int WEEKDAYS = 5;
-    System.out.println("Введите резюме дефекта:");
     Scanner scanner = new Scanner(System.in);
-    String resume = scanner.nextLine();
-    System.out.println("Укажите критичность дефекта" + "\nВысокий" + "\nСредний" + "\nНизкий");
-    String priority = scanner.nextLine();
-    System.out.println("Укажите ожидаемое количество дней на исправление дефекта");
-    int days = scanner.nextInt();
-    scanner.nextLine();
-    boolean moreThanWeek = days > WEEKDAYS;
-    System.out.println(
-        "РЕЗЮМЕ: " + resume + "\nКРИТИЧНОСТЬ: " + priority + "\nВРЕМЯ ИСПРАВЛЕНИЯ: " + days
-            + "\nБОЛЬШЕ РАБОЧЕЙ НЕДЕЛИ? " + moreThanWeek);
-
+    String[] array_bugs = new String[10];
+    String playProgram = "Выберете операцию:" + "\n1 - Add" + "\n2 - List" + "\n3 - Quit";
+    int operation;
+    int i = 0;
+    do {
+      System.out.println(playProgram);
+      operation = scanner.nextInt();
+      scanner.nextLine();
+      switch (operation) {
+        case 1:
+          if (i < array_bugs.length) {
+            System.out.println("Введите резюме дефекта:");
+            String resume = scanner.nextLine();
+            System.out.println(
+                "Укажите критичность дефекта:" + "\nВысокий" + "\nСредний" + "\nНизкий");
+            String priority = scanner.nextLine();
+            System.out.println("Укажите ожидаемое количество дней на исправление дефекта:");
+            int days = scanner.nextInt();
+            scanner.nextLine();
+            String bugs = i + " | " + resume + " | " + priority + " | " + days;
+            array_bugs[i] = bugs;
+            i++;
+          } else {
+            System.out.println("Вы добавили максимально-возможное количество дефектов.");
+          }
+          break;
+        case 2:
+          System.out.println(Arrays.toString(array_bugs));
+          break;
+        case 3:
+          System.out.println("GoodBye!");
+          break;
+        default:
+          System.out.println("Введена неверная операция! Повторите попытку");
+      }
+    }
+    while ((operation != 3));
   }
 }
