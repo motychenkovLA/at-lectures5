6a81ec78bac7217fbe361a966d94c0145fdeeaa1
lmotychenkov <motychenkovLA@gmail.com>
2021-10-11 11:02:20 +0300

    дз 3 замечания

--- src/tracker/Main.java
+++ src/tracker/Main.java
@@ -5,17 +5,24 @@ import java.util.Scanner;
 
 public class Main {
 
+  // todo 0 - 4 пробела отступ плз
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
+    // todo 3 - в памяти хранится не дефект, а его консольное представление
+    //  лучше сделать по массиву на каждое поле дефекта
+    // todo 3 - 10 это константа
+    // todo 1 - используется _ в названии
     String[] array_bugs = new String[10];
     String playProgram = "Выберете операцию:" + "\n1 - Add" + "\n2 - List" + "\n3 - Quit";
     int operation;
+    // todo 0 - что значит "i" ?
     int i = 0;
     do {
       System.out.println(playProgram);
       operation = scanner.nextInt();
       scanner.nextLine();
       switch (operation) {
+        // todo ? - возможно лучше все таки словами
         case 1:
           if (i < array_bugs.length) {
             System.out.println("Введите резюме дефекта:");
@@ -34,6 +41,7 @@ public class Main {
           }
           break;
         case 2:
+          // todo 3 - выводятся null-ы в незаполненных ячейках, нужно выводить только те дефекты что есть
           System.out.println(Arrays.toString(array_bugs));
           break;
         case 3:
@@ -41,7 +49,11 @@ public class Main {
           break;
         default:
           System.out.println("Введена неверная операция! Повторите попытку");
+          // todo 3 - break;
       }
+      // todo 1 - } while (...); - на одной строке
+      // todo 3 - дублирование проверки, которая делается в case;
+      //   лучше ввести переменную вроде runProgram и менять ее в нужном case
     }
     while ((operation != 3));
   }
