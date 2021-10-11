ebe832c397220f8e48ed88b83db37cc1be34afd6
chernyshevPV <isutaru99@gmail.com>
2021-10-11 10:52:22 +0300

    ДЗ 3

---
+++ src/tracker/Main.java
@@ -0,0 +1,19 @@
+package tracker;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите резюме дефекта:");
+        String summary = scanner.nextLine();
+        System.out.println("Введите критичность дефекта:\n" + "Критичный\n" + "Высокий\n" + "Средний\n" + "Низкий");
+        String criticality = scanner.nextLine();
+        System.out.println("Введите кол-во дней на исправление дефекта:");
+        int days = scanner.nextInt();
+        scanner.nextLine();
+        boolean border;
+        border = days > 5;
+        System.out.println("Результат:\n" + "Резюме дефекта - " + summary + "\n" + "Критичность дефекта - " + criticality + "\n" + "Кол-во дней на исправление - " + days + "\n" + "Займет больше рабочей недели - " + border);
+    }
+}
---
+++ src/trackerV2/Main.java
@@ -0,0 +1,54 @@
+package trackerV2;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        final int ARRAY_SIZE = 10;
+        String[] list = new String[ARRAY_SIZE];
+        int i = 0;
+        int a = 0;
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите название необходимой опции:\n" + "Add\n" + "List\n" + "Quit");
+        String option = scanner.nextLine();
+
+        while (!"Quit".equals(option)) {
+
+            switch (option) {
+                case "Add":
+                    if (i != 10) {
+
+                        System.out.println("Введите резюме дефекта:");
+                        String summary = scanner.nextLine();
+                        System.out.println("Введите критичность дефекта:\n" + " - Критичный\n" + " - Высокий\n" + " - Средний\n" + " - Низкий");
+                        String criticality = scanner.nextLine();
+                        System.out.println("Введите кол-во дней на исправление дефекта:");
+                        int days = scanner.nextInt();
+                        scanner.nextLine();
+                        String output = String.join(" ", "\nРезюме дефекта: " + summary + "\n" + "Критичность дефекта: " + criticality + "\n" + "Кол-во дней на исправление: " + days + "\n");
+                        list[i] = output;
+                        i++;
+                        System.out.println("Введите название необходимой опции:\n" + "Add\n" + "List\n" + "Quit");
+                        option = scanner.nextLine();
+                    } else {
+                        System.out.println("Лимит превышен!\n" + "Введите название необходимой опции:\n" + "Add\n" + "List\n" + "Quit");
+                        option = scanner.nextLine();
+                    }
+                    break;
+                case "List":
+                    while (a != i) {
+                        System.out.println("Номер: " + a + list[a]);
+                        a++;
+                    }
+                    System.out.println("\nВведите название необходимой опции:\n" + "Add\n" + "List\n" + "Quit");
+                    option = scanner.nextLine();
+                    break;
+
+            }
+
+        }
+    }
+
+}
