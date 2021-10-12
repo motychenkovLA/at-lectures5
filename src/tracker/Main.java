6c4790257922a7afb643713b9932825969d55ff3
schupikova <victorovna.sveta@gmail.com>
2021-10-12 18:16:22 +0300

    ДЗ_3. Исправления. Применен GoogleCodeStyle

--- src/tracker/Main.java
+++ src/tracker/Main.java
@@ -5,56 +5,58 @@ import java.util.Scanner;
 
 public class Main {
 
-  // todo 0 - 4 пробела отступ плз
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    // todo 3 - в памяти хранится не дефект, а его консольное представление
-    //  лучше сделать по массиву на каждое поле дефекта
-    // todo 3 - 10 это константа
-    // todo 1 - используется _ в названии
-    String[] array_bugs = new String[10];
-    String playProgram = "Выберете операцию:" + "\n1 - Add" + "\n2 - List" + "\n3 - Quit";
-    int operation;
-    // todo 0 - что значит "i" ?
-    int i = 0;
+    final int arraySize = 10;
+    String[] arrayResume = new String[arraySize];
+    String[] arrayPriority = new String[arraySize];
+    int[] arrayDays = new int[arraySize];
+    String playProgram = "Выберете операцию:" + "\nAdd" + "\nList" + "\nQuit";
+    String operation;
+    String goodBye = "Quit";
+    int index = 0;
     do {
       System.out.println(playProgram);
-      operation = scanner.nextInt();
-      scanner.nextLine();
+      operation = scanner.nextLine();
       switch (operation) {
-        // todo ? - возможно лучше все таки словами
-        case 1:
-          if (i < array_bugs.length) {
+        case "Add":
+          if (index < arraySize) {
             System.out.println("Введите резюме дефекта:");
-            String resume = scanner.nextLine();
+            arrayResume[index] = scanner.nextLine();
             System.out.println(
                 "Укажите критичность дефекта:" + "\nВысокий" + "\nСредний" + "\nНизкий");
-            String priority = scanner.nextLine();
+            arrayPriority[index] = scanner.nextLine();
             System.out.println("Укажите ожидаемое количество дней на исправление дефекта:");
-            int days = scanner.nextInt();
+            arrayDays[index] = scanner.nextInt();
             scanner.nextLine();
-            String bugs = i + " | " + resume + " | " + priority + " | " + days;
-            array_bugs[i] = bugs;
-            i++;
+            index++;
           } else {
             System.out.println("Вы добавили максимально-возможное количество дефектов.");
           }
           break;
-        case 2:
-          // todo 3 - выводятся null-ы в незаполненных ячейках, нужно выводить только те дефекты что есть
-          System.out.println(Arrays.toString(array_bugs));
+        case "List":
+          for (int indSearch = 0; indSearch < arraySize; indSearch++) {
+            if (arrayResume[indSearch] != null) {
+              System.out.println(
+                  "Номер:" + indSearch + " Резюме: " + arrayResume[indSearch] + " Приоритет: "
+                      + arrayPriority[indSearch] + " Дней на исправление: " + arrayDays[indSearch]
+                      + "\n");
+            } else if (arrayResume[indSearch] == null && indSearch != 0) {
+              break;
+            } else {
+              System.out.println("Заведенные дефекты отсутствуют");
+              break;
+            }
+          }
           break;
-        case 3:
+        case "Quit":
           System.out.println("GoodBye!");
           break;
         default:
           System.out.println("Введена неверная операция! Повторите попытку");
-          // todo 3 - break;
+          break;
       }
-      // todo 1 - } while (...); - на одной строке
-      // todo 3 - дублирование проверки, которая делается в case;
-      //   лучше ввести переменную вроде runProgram и менять ее в нужном case
-    }
-    while ((operation != 3));
+    } while (!(operation.equalsIgnoreCase(goodBye)));
+
   }
 }
