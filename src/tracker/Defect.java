fd1d9f4d1ac0f00ddcc7ecb1f568fa303c45720c
Pronnikov <apronnikov@t1-consulting.ru>
2021-10-25 16:13:31 +0300

    дз4 принято

--- src/tracker/Defect.java
+++ src/tracker/Defect.java
@@ -1,6 +1,5 @@
 package tracker;
 
-//todo переименовать класс в Defect как указанно в задании
 public class Defect {
 
     String resume;
@@ -9,7 +8,6 @@ public class Defect {
     long id ;
 
 
-//todo первый аргумент должен называться id
     public Defect (long id, String resume, String priority, int daysToFixed) {
         this.resume = resume;
         this.priority = priority;
@@ -18,7 +16,6 @@ public class Defect {
     }
 
 
-//todo исправить warning
     public String getInfo(){
         return "|Id:" + id + "|Резюме:" + resume  + "|Приоритет:" + priority + "|Количество дней на исправление:"
                 + daysToFixed + "|";
