0c906b23a6a6f1ceca429159bcba4f8e7d24eec7
UliaSavina <ulia_nna@mail.ru>
2021-11-05 21:15:46 +0300

    ДЗ 4 испр опечатки

--- src/tracker/Defect.java
+++ src/tracker/Defect.java
@@ -3,18 +3,18 @@ public class Defect {
 
     long id;
     String summary;
-    String severuty;
+    String severity;
     int daysToFix;
 
-    public Defect(long id, String summary, String severuty, int daysToFix) {
+    public Defect(long id, String summary, String severity, int daysToFix) {
         this.id = id;
         this.summary = summary;
-        this.severuty = severuty;
+        this.severity = severity;
         this.daysToFix = daysToFix;
     }
 
     public String getInfo () {
-        return "ID:" + id + " | " + "РЕЗЮМЕ:" + summary + " | " + "СЕРЬЕЗНОСТЬ:" + severuty + " | " +
+        return "ID:" + id + " | " + "РЕЗЮМЕ:" + summary + " | " + "СЕРЬЕЗНОСТЬ:" + severity + " | " +
                 "КОЛИЧЕСТВО ДНЕЙ НА ИСПРАВЛЕНИЕ:" + daysToFix;
 
     }
