7b4f23215d5199446aa462c0b3fcbd1c04c98147
Vladimir Yalovenko <vindager.prod@yandex.ru>
2021-11-07 23:50:43 +0300

    DZ_3 = Предложение пользователю - Добавить новый дефект ("add"), вывести список ("list") или выйти из программы ("quit").        Открывается главное меню после добавления данных.        Программа может хранить 10 дефектов, при попытке добавить больше, сообщает о невозможности и автоматически возвращает в главное меню.        При выводе списка дефекты отображаются в виде "номер (начиная с 0) | резюме | серьезность | количество дней на исправление".        При вводе некоретного пункта меню - сообщение.

--- src/tracker/Main.java
+++ src/tracker/Main.java
@@ -6,22 +6,94 @@ public class Main {
 
     public static void main(String[] args) {
 
-        final int WORKING_WEEK = 5;
+        final int BUG_QUANTITY = 10;
 
-        Scanner data_bug = new Scanner(System.in);
+        boolean bug_tracker_launched = true;
 
-        System.out.print("Введите резюме дефекта: ");
-        String resume_bug = data_bug.nextLine();
+        int base_bug_quantity = 0;
 
-        System.out.print("Введите критичность дефекта одним значением (Низкий, Средний, Высокий, Блокирующий): ");
-        String criticality_bug = data_bug.next();
+        String[] solid_bug_resume = new String[BUG_QUANTITY];
 
-        System.out.print("Введите количество дней для исправления дефекта в числовом формате: ");
-        int days_fix_bug = data_bug.nextInt();
+        String[] solid_bug_criticality = new String[BUG_QUANTITY];
 
-        boolean comp_days_bug = days_fix_bug > WORKING_WEEK;
+        int[] solid_bug_fix_days = new int[BUG_QUANTITY];
 
-        String generalInfo_bug = resume_bug + " | " + criticality_bug + " | " + days_fix_bug + " | " + "Займет больше одной недели:" + "(" + comp_days_bug + ")";
-        System.out.println("Общая информация по дефекту: " + generalInfo_bug);
+        Scanner base_bug = new Scanner(System.in);
+
+        while (bug_tracker_launched) {
+
+            System.out.println("Меню баг-трекинговой системы:"
+                    + System.lineSeparator() + "1. Add"
+                    + System.lineSeparator() + "2. List"
+                    + System.lineSeparator() + "3. Quit");
+
+            System.out.print("Введите цифровой номер требуемого пункта: ");
+
+            int bug_menu_num = base_bug.nextInt();
+
+            switch (bug_menu_num) {
+
+                case 1: {
+
+                    if (base_bug_quantity < BUG_QUANTITY) {
+
+                        base_bug.nextLine();
+
+                        System.out.print("Введите резюме дефекта: ");
+                        String bug_resume = base_bug.nextLine();
+
+                        System.out.print("Введите критичность дефекта одним значением (Низкий, Средний, Высокий, Блокирующий): ");
+                        String bug_criticality = base_bug.next();
+
+                        System.out.print("Введите количество дней для исправления дефекта в числовом формате: ");
+                        int bug_fix_days = base_bug.nextInt();
+
+                        solid_bug_resume[base_bug_quantity] = bug_resume;
+
+                        solid_bug_criticality[base_bug_quantity] = bug_criticality;
+
+                        solid_bug_fix_days[base_bug_quantity] = bug_fix_days;
+
+                        base_bug_quantity++;
+
+                    } else {
+
+                        System.out.println("Достигнуто максимальное значение заведенных дефектов кратное: " + "(" + BUG_QUANTITY + ")");
+                    }
+                }
+                break;
+
+                case 2: {
+
+                    if (base_bug_quantity > 0) {
+
+                        for (int i = 0; i < base_bug_quantity; i++) {
+
+                            System.out.println("Номер дефекта: " + i + " | "
+                                    + "Резюме дефекта: " + solid_bug_resume[i] + " | "
+                                    + "Приоритет дефекта: " + solid_bug_criticality[i] + " | "
+                                    + "Колличетсво дней на исправление дефекта: " + solid_bug_fix_days[i]);
+                        }
+                    } else {
+
+                        System.out.println("ПУСТО");
+
+                    }
+                }
+                break;
+
+                case 3: {
+
+                    System.out.println("Завершение работы баг-трегинговой системы");
+
+                    bug_tracker_launched = false;
+                }
+                default: {
+
+                    System.out.println("Не корректное значение пункта! Введите номер пункта согласно меню!");
+
+                }
+            }
+        }
     }
 }
